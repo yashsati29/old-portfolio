6cff98725eb0d27d9795f2ac28ca783c25137b7a
Yash Kumar Sati <35272825+yashsati29@users.noreply.github.com>
2019-05-06 17:58:25 +0530

    Added functionality for loaderText to disappear after loading!

--- js/script.js
+++ js/script.js
@@ -2,36 +2,38 @@
 
 // OFF CANVAS USER-SCRIPT
 function openSlideMenu() {
-	document.getElementById('side-menu').style.width = '140px';
-	document.getElementById('main').style.marginLeft = '140px';
+  document.getElementById('side-menu').style.width = '140px';
+  document.getElementById('main').style.marginLeft = '140px';
 }
 
 function closeSlideMenu() {
-	document.getElementById('side-menu').style.width = '0';
-	document.getElementById('main').style.marginLeft = '0';
+  document.getElementById('side-menu').style.width = '0';
+  document.getElementById('main').style.marginLeft = '0';
 }
 
 // PRELOADER USER-SCRIPT
-if (screen.width >= 1000) {     
-	var myVar;
+if (screen.width >= 1000) {
+  var myVar;
 
-	function myFunction() {
-		myVar = setTimeout(showPage, 3000);
-	}
+  function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+  }
 
-	function showPage() {
-		document.getElementById("loader").style.display = "none";
-		document.getElementById("myDiv").style.display = "block";
-	}
-}else {
-	var myVar;
+  function showPage() {
+    document.getElementById("loaderText").style.display = "none";
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+  }
+} else {
+  var myVar;
 
-	function myFunction() {
-		myVar = setTimeout(showPage, 0);
-	}
+  function myFunction() {
+    myVar = setTimeout(showPage, 0);
+  }
 
-	function showPage() {
-		document.getElementById("loader").style.display = "none";
-		document.getElementById("myDiv").style.display = "block";
-	}
+  function showPage() {
+    document.getElementById("loaderText").style.display = "none";
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+  }
 }
